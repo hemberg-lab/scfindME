645544144fadb02096636b4fc2e5dd0ea9101d48
YY-SONG0718 <794815059@qq.com>
2020-09-03 03:38:56 +0000

    example data

---
+++ R/Tasic_2016.R
@@ -0,0 +1,15 @@
+#' Example SCFind alternative splicing index
+#' `Tasic_2016` Example SCFind alternative splicing index data for mouse glutamatergic and GABA-ergic neurons
+#' @docType data
+#'
+#' @usage data(Tasic_2016)
+#'
+#' @format url to retrieve SCfind indexes.
+#'
+#' @keywords index
+#' 
+#' @source \url{/lustre/scratch117/cellgen/team218/ys6/Tasic_data}
+#'
+#' @references Allan Brain Atlas (https://www.nature.com/articles/nn.4216)).
+
+"Tasic_2016"
